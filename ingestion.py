5feb4a34fa87e835341779d99749cbee7ccfb5e3
ondrej.ploteny <ondrej.ploteny@thermofisher.com>
2023-11-17 12:20:21 +0100

    add logging to ingestion

--- ingestion.py
+++ ingestion.py
@@ -1,9 +1,12 @@
 import pandas as pd
 import numpy as np
 import os
+import sys
 import json
 from datetime import datetime
+import logging
 
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 
 # Load config.json and get input and output paths
 with open('config.json', 'r') as f:
@@ -35,23 +38,30 @@ def merge_multiple_dataframe():
     """
     final_dataset_log = list()
     final_dataset = pd.DataFrame()
+    final_dataset_path = os.path.join(output_folder_path, 'finaldata.csv')
+    final_log_path = os.path.join(output_folder_path, 'ingestedfiles.txt')
 
     for dataset_path in locate_datasets(input_folder_path):
         curr_dataset = pd.read_csv(dataset_path)
         final_dataset = pd.concat([final_dataset, curr_dataset], ignore_index=True)
         curr_timestamp = datetime.now().strftime('%d/%m/%Y-%H:%M:%S')
         final_dataset_log.append(f"{curr_timestamp} {dataset_path}")
+        logging.info(f"STEP: ingestion, partial dataset loaded {dataset_path}")
 
     final_dataset.drop_duplicates(inplace=True)
 
     if not os.path.exists(output_folder_path):
         os.makedirs(output_folder_path)
 
-    final_dataset.to_csv(os.path.join(output_folder_path, 'finaldata.csv'), index=False)
+    final_dataset.to_csv(final_dataset_path, index=False)
+    logging.info(f"STEP: ingestion, dataset dumped to {final_dataset_path}")
 
-    with open(os.path.join(output_folder_path, 'ingestedfiles.txt'), "w") as file:
+    with open(final_log_path, "w") as file:
         file.write("\n".join(final_dataset_log))
+        logging.info(f"STEP: ingestion, log dumped to {final_log_path}")
 
 
 if __name__ == '__main__':
+    logging.info("STEP: ingestion, begin")
     merge_multiple_dataframe()
+    logging.info("STEP: ingestion, done")
