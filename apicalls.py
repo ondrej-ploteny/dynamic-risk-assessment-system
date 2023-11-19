47ecc7f6aee4263e67d83b18993d12602e031df5
ondrej.ploteny <ondrej.ploteny@thermofisher.com>
2023-11-19 16:54:13 +0100

    implement flask api and test

--- apicalls.py
+++ apicalls.py
@@ -1,20 +1,51 @@
+import json
+import os
+import pandas as pd
 import requests
 
-#Specify a URL that resolves to your workspace
-URL = "http://127.0.0.1/"
+# Specify a URL that resolves to your workspace
+URL = "http://127.0.0.1:8000"
+
+# Get test data:
+# Load config.json and get path variables
+with open('config.json', 'r') as f:
+    config = json.load(f)
+
+# path to data used for analysis
+test_data_path = os.path.join(config['test_data_path'], 'testdata.csv')
+report_path = os.path.join(config['output_model_path'], 'apireturns.txt')
 
 
+def api_call():
+    header = {"Content-Type": "application/json"}
+    body = {'filepath': test_data_path}
+    response1 = requests.post(
+        url=URL + '/prediction',
+        headers=header,
+        json=body
+    ).json()
 
-#Call each API endpoint and store the responses
-response1 = #put an API call here
-response2 = #put an API call here
-response3 = #put an API call here
-response4 = #put an API call here
+    # scoring
+    response2 = requests.get(URL + '/scoring').json()
 
-#combine all API responses
-responses = #combine reponses here
+    # statistics
+    response3 = requests.get(URL + '/summarystats').json()
 
-#write the responses to your workspace
+    # diagnostics
+    response4 = requests.get(URL + '/diagnostics').json()
+
+    data = [
+        {'key': 'prediction', 'value': response1},
+        {'key': 'scoring', 'value': response2},
+        {'key': 'statistics', 'value': response3},
+        {'key': 'missing pct', 'value': response4['missing']},
+        {'key': 'execution time', 'value': response4['time_check']},
+        {'key': 'outdated packages', 'value': response4['outdated']}
+    ]
+
+    api_report = pd.DataFrame(data, columns=['key', 'value'])
+    api_report.to_csv(report_path, index=False, header=False, sep='\t')
 
 
-
+if __name__ == "__main__":
+    api_call()
--- app.py
+++ app.py
@@ -1,20 +1,15 @@
 from flask import Flask, session, jsonify, request
 import pandas as pd
-import numpy as np
-import pickle
-import create_prediction_model
-import diagnosis 
-import predict_exited_from_saved_model
 import json
 import os
+from diagnostics import model_predictions, dataframe_summary, missing_data, execution_time, outdated_packages_list
+from scoring import score_model
 
-
-
-######################Set up variables for use in our script
+# Set up variables for use in our script
 app = Flask(__name__)
 app.secret_key = '1652d576-484a-49fd-913a-6879acfa6ba4'
 
-with open('config.json','r') as f:
+with open('config.json', 'r') as f:
     config = json.load(f) 
 
 dataset_csv_path = os.path.join(config['output_folder_path']) 
@@ -22,29 +17,60 @@ dataset_csv_path = os.path.join(config['output_folder_path'])
 prediction_model = None
 
 
-#######################Prediction Endpoint
-@app.route("/prediction", methods=['POST','OPTIONS'])
-def predict():        
-    #call the prediction function you created in Step 3
-    return #add return value for prediction outputs
+@app.route("/prediction", methods=['POST', 'OPTIONS'])
+def predict():
+    """
+    call the prediction function you created in Step 3
+    add return value for prediction outputs
+    :return:
+    """
+    dataset_path = request.get_json()['filepath']
 
-#######################Scoring Endpoint
-@app.route("/scoring", methods=['GET','OPTIONS'])
-def stats():        
-    #check the score of the deployed model
-    return #add return value (a single F1 score number)
+    predictor_column_name = ['lastmonth_activity',
+                             'lastyear_activity',
+                             'number_of_employees']
 
-#######################Summary Statistics Endpoint
-@app.route("/summarystats", methods=['GET','OPTIONS'])
-def stats():        
-    #check means, medians, and modes for each column
-    return #return a list of all calculated summary statistics
+    df = pd.read_csv(dataset_path, usecols=predictor_column_name)
+    y_pred = model_predictions(df)
+    return jsonify(y_pred)
 
-#######################Diagnostics Endpoint
-@app.route("/diagnostics", methods=['GET','OPTIONS'])
-def stats():        
-    #check timing and percent NA values
-    return #add return value for all diagnostics
+
+@app.route("/scoring", methods=['GET', 'OPTIONS'])
+def stats():
+    """
+    Scoring Endpoint
+    check the score of the deployed model
+    :return: a single F1 score number
+    """
+
+    f1_score = score_model(is_dump=False)
+    return jsonify([f1_score])
+
+
+@app.route("/summarystats", methods=['GET', 'OPTIONS'])
+def summary():
+    """
+    Summary Statistics Endpoint
+    :return:
+    """
+    col_stats = dataframe_summary()
+    return jsonify(col_stats)
+
+
+@app.route("/diagnostics", methods=['GET', 'OPTIONS'])
+def diagnostics():
+    """
+    Diagnostics Endpoint
+    :return:
+    """
+    diagnostics_dict = {
+        'missing': missing_data(),
+        'time_check': execution_time(),
+        'outdated': outdated_packages_list()
+    }
+
+    return jsonify(diagnostics_dict)
+
 
 if __name__ == "__main__":    
     app.run(host='0.0.0.0', port=8000, debug=True, threaded=True)
